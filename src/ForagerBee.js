4563e3bbba8a98d23c512398edf3cc214b7e655d
Wendy Huynh <wendyhuynhs@gmail.com>
2019-04-15 10:51:24 -0700

    Completed and passed all specs

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype); //Creates an empty object that has access to bee's prototypes
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(value) {
+  this.treasureChest.push(value);
+};
